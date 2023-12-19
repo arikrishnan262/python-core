f30f781265e5d99582efbfda8a6cb8d5d20f904d
Arikrishnan <146700985+arikrishnan262@users.noreply.github.com>
2023-12-19 10:33:21 +0530

    Add files via upload

---
+++ conditiongreater.py
@@ -0,0 +1,6 @@
+x=5
+y=12
+if x> y:
+    print("x is greater")
+else:
+    print("y is greater or equal")   
---
+++ dateconverted.py
@@ -0,0 +1,4 @@
+today = date.today()
+str = date.isofarmat(today)
+print("string Representation",str)
+print(type(str))
