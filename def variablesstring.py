6e5eb29676f0867a7ee6e4f720f16d29aeb66935
Arikrishnan <146700985+arikrishnan262@users.noreply.github.com>
2023-12-19 10:34:21 +0530

    Add files via upload

---
+++ def variablesstring.py
@@ -0,0 +1,11 @@
+def f():
+       
+       hai="i am"
+       name="seenu"
+       frome="tamilnadu"
+       stringconcatenation=hai+""+name+""+frome
+       print(stringconcatenation)
+
+# Drive code
+f()
+
